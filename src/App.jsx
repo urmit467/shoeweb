293316f86b0ec68b7175359906fe2248fbeaa7e4
Urmit Kaundal <urmitkaundal467@gmail.com>
2025-09-03 23:52:23 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Frontpage from './components/Frontpage'
 import './App.css'
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 
   return (
  <>
-
+  <Frontpage />
  </>
   )
 }
--- src/components/frontpage.jsx
+++ src/components/frontpage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const frontpage = () => {
+const Frontpage = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 100); 
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="relative h-72 overflow-hidden bg-white">
+      {/* Shoe image */}
+      <img 
+        className={`
+          absolute left-150 bottom-0 w-84 h-84 z-0 rotate-5 transform -translate-x-1/2 transition-all duration-1000 ease-out
+          ${show ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0'}
+        `}
+        src="src/components/images/openingshoe.png" 
+        alt="loading..." 
+      />
 
-export default frontpage
+      {/* Text */}
+      <p className="
+        relative z-10 text-black text-9xl font-bold text-center pt-40 bottom-10
+      ">
+        <i>Stylish <span className="text-blue-500">footwear</span></i>
+      </p>
+    </div>
+  );
+};
+
+export default Frontpage;
